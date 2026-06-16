6cbd88b16b6da7a1c27031e185190af0b0a9ad5b
mominaabid <mominaabid789@gmail.com>
2026-06-16 10:22:41 +0500

    y

--- server.ts
+++ server.ts
@@ -136,15 +136,12 @@ app.get("/cloudinary-test", async (req, res) => {
   }
 });
 
-// app.listen(PORT, () => {
-//   console.log(`Backend is running on ${PORT}`);
-// });
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Backend running on http://0.0.0.0:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Backend is running on ${PORT}`);
 });
-
 // app.listen(PORT, "0.0.0.0", () => {
-//   console.log(`Server running  on http://192.168.1.3:${PORT}`);
+//   console.log(`Backend running on http://0.0.0.0:${PORT}`);
 // });
+
 
 export default app;
